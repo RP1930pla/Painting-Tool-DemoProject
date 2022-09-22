8ae7c9475faea8df73f814623720520da9481e99
RP1930pla <47044476+RP1930pla@users.noreply.github.com>
2022-09-22 20:16:37 +0200

    UI implementation

--- Grass Painter Project/Assets/3D Cursor/CursorInstancer.cs
+++ Grass Painter Project/Assets/3D Cursor/CursorInstancer.cs
@@ -1,17 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.SceneManagement;
 
 public class CursorInstancer : MonoBehaviour
 {
     private Vector3 HitNormalGiz;
     private Vector3 HitPosGiz;
-    
+    private int LayerUI;
     [Header("References")]
     public GameObject Gizmo;
     public Camera Cam;
 
+
+    private void Start()
+    {
+        //LayerUI = LayerMask.NameToLayer("UI");
+    }
     void Update()
     {
         RaycastHit hit;
@@ -23,16 +29,33 @@ public class CursorInstancer : MonoBehaviour
         
         if (Physics.Raycast(Cam.ScreenPointToRay(Input.mousePosition),out hit))
         {
-            Collider coll = hit.collider;
-            HitNormalGiz = hit.normal;
-            HitPosGiz = hit.point;
-
-            if (coll != null)
+            if (EventSystem.current.IsPointerOverGameObject() == false)
             {
-                Gizmo.transform.position = HitPosGiz;
-                Gizmo.transform.rotation = Quaternion.FromToRotation(Vector3.up, hit.normal);
+                Collider coll = hit.collider;
+                HitNormalGiz = hit.normal;
+                HitPosGiz = hit.point;
+
+                //Collider coll = hit.collider;
+                //HitNormalGiz = hit.normal;
+                //HitPosGiz = hit.point;
+
+                if (coll != null)
+                {
+                    Gizmo.transform.position = HitPosGiz;
+                    Gizmo.transform.rotation = Quaternion.FromToRotation(Vector3.up, hit.normal);
+                }
+            }
+
+            if(EventSystem.current.IsPointerOverGameObject() == true)
+            {
+                //Debug.Log("UIIII");
+                Gizmo.transform.position = new Vector3(6.27f, 27.96f, -1.23f);
+                Gizmo.transform.rotation = Quaternion.Euler(new Vector3(0f, 0f, 0f));
             }
         }
 
     }
+
+
+
 }
